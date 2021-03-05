689797b65a9b69820dd2cf1f667b9b0fcfc5eba4
yoshioka <yoshioka0627@gmail.com>
2021-03-06 08:17:47 +0900

    :recycle: UMLを変数化 各デフォルト値をrootからの相対パスに修正

--- bin/svg.sh
+++ bin/svg.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-SVG=${SVG:-model.svg}
+UML=${UML:-./docs/ticket.uml}
+SVG=${SVG:-./docs/model.svg}
 TMP=${TMP:-tmp}
 
 rm ${SVG}
 
-curl -X POST http://localhost:8888/form -d "text=$(cat ticket.uml)" -D - > ${TMP}
+curl -X POST http://localhost:8888/form -d "text=$(cat ${UML})" -D - > ${TMP}
 
 wget -O ${SVG} $(cat ${TMP} | grep Location | sed -e 's/Location: //g' | sed -e 's/\/uml/\/svg/g')
 
